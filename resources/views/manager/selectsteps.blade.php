41f98188972156382da938306b0aa62c18673c7d
Abhinav Sharma <19co01@aitdgoa.edu.in>
2022-12-14 21:51:39 +0530

    Removed strike through after after a step is check marked

--- resources/views/manager/selectsteps.blade.php
+++ resources/views/manager/selectsteps.blade.php
@@ -44,7 +44,7 @@
                                     <label for="todoCheck1"></label>
                                 </div>
                                 <!-- todo text -->
-                                <span class="text">STEP-1</span>
+                                <span>STEP-1</span>
                                 <!-- Emphasis label -->
                                 {{-- <small class="badge badge-danger"><i class="far fa-clock"></i> 2 mins</small> --}}
                                 <!-- General tools such as edit or delete-->
@@ -56,7 +56,7 @@
                                     <input type="checkbox" value="" name="todo2" id="todoCheck2" checked>
                                     <label for="todoCheck2"></label>
                                 </div>
-                                <span class="text">STEP-2</span>
+                                <span>STEP-2</span>
                                 {{-- <small class="badge badge-info"><i class="far fa-clock"></i> 5 mins</small> --}}
                                 
                             </li>
@@ -66,7 +66,7 @@
                                     <input type="checkbox" value="" name="todo3" id="todoCheck3" checked>
                                     <label for="todoCheck3"></label>
                                 </div>
-                                <span class="text">STEP-3</span>
+                                <span>STEP-3</span>
                                 {{-- <small class="badge badge-warning"><i class="far fa-clock"></i> 10 mins</small> --}}
                                 
                             </li>
@@ -76,7 +76,7 @@
                                     <input type="checkbox" value="" name="todo4" id="todoCheck4" checked>
                                     <label for="todoCheck4"></label>
                                 </div>
-                                <span class="text">STEP-4</span>
+                                <span>STEP-4</span>
                                 {{-- <small class="badge badge-success"><i class="far fa-clock"></i> 15 mins</small> --}}
                                 
                             </li>
@@ -86,7 +86,7 @@
                                     <input type="checkbox" value="" name="todo5" id="todoCheck5" checked>
                                     <label for="todoCheck5"></label>
                                 </div>
-                                <span class="text">STEP-5</span>
+                                <span>STEP-5</span>
                                 {{-- <small class="badge badge-primary"><i class="far fa-clock"></i> 20 mins</small> --}}
                                
                             </li>
@@ -96,7 +96,7 @@
                                     <input type="checkbox" value="" name="todo6" id="todoCheck6" checked>
                                     <label for="todoCheck6"></label>
                                 </div>
-                                <span class="text">STEP-6</span>
+                                <span>STEP-6</span>
                                 {{-- <small class="badge badge-secondary"><i class="far fa-clock"></i> 1 hr</small> --}}
                                 
                             </li>
